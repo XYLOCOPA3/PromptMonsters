e4d5547c62c33bf71c220bd2daaa87774470ff42
keit 🧶 <keit0728.job@gmail.com>
2023-04-19 14:52:46 +0900

    FixEnv

--- src/const/contract.ts
+++
@@ -1,2 +0,0 @@
-export const PROMPT_MONSTER_ADDRESS = process.env
-  .PROMPT_MONSTERS_CONTRACT as string;
--- src/features/monster/api/contracts/client/ClientPromptMonstersContract.ts
+++ src/features/monster/api/contracts/client/ClientPromptMonstersContract.ts
@@ -1,4 +1,3 @@
-import { PROMPT_MONSTER_ADDRESS } from "@/const/contract";
 import { BasePromptMonstersContract } from "@/features/monster/api/contracts/BasePromptMonstersContract";
 import { ClientWallet } from "@/lib/wallet";
 import { MonsterModel } from "@/models/MonsterModel";
@@ -26,17 +25,13 @@ export class ClientPromptMonstersContract extends BasePromptMonstersContract {
   public static async instance(): Promise<ClientPromptMonstersContract> {
     const wallet = await ClientWallet.instance();
     const reader = PromptMonsters__factory.connect(
-      PROMPT_MONSTER_ADDRESS,
-      // "0xA821411988D5E9dbf6614fDd1eD81d19392e0b93", // mumbai
-      // "0x2fB0455276cB8577458a97033E9cb7245aa3CAD3", // linea
+      process.env.NEXT_PUBLIC_PROMPT_MONSTERS_CONTRACT!,
       wallet.provider,
     );
     return new ClientPromptMonstersContract(
       wallet,
       reader,
-      PROMPT_MONSTER_ADDRESS,
-      // "0xA821411988D5E9dbf6614fDd1eD81d19392e0b93", // mumbai
-      // "0x2fB0455276cB8577458a97033E9cb7245aa3CAD3", // linea
+      process.env.NEXT_PUBLIC_PROMPT_MONSTERS_CONTRACT!,
     );
   }
 
