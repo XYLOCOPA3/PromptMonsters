b7a73ab095c9a81e0e2e127c97b8f9af23ca6649
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-04-25 01:35:07 +0900

    feat: add token scripts

---
+++ contracts/prompt-monsters/scripts/Mock/MCHCoinTransfer.ts
@@ -0,0 +1,14 @@
+import { MCHC_PROXY_ADDRESS } from "../const";
+import { ethers } from "hardhat";
+
+export async function transferMCHCoin() {
+  const MCHCoin = await ethers.getContractFactory("MCHCoin");
+
+  const mchCoin = MCHCoin.attach(MCHC_PROXY_ADDRESS);
+
+  const address: string = "0x31F31693723c4397cb8A978A19A95B82c72f4212";
+
+  (await mchCoin.transfer(address, ethers.utils.parseEther("2000"))).wait();
+
+  console.log(await mchCoin.balanceOf(address));
+}
