7a5866b3cfb88495c6d1219fb5610fd587df1107
yosuke <yosuke2002jp@yahoo.co.jp>
2023-05-30 01:32:47 +0900

    modify present-per-mint.ts

--- contracts/prompt-monsters/scripts/campaignMay2023/present-per-mint.ts
+++ contracts/prompt-monsters/scripts/campaignMay2023/present-per-mint.ts
@@ -1,4 +1,5 @@
 import { PROMPT_MONSTERS_PROXY_ADDRESS } from "../const";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
 export async function main() {
@@ -10,6 +11,70 @@ export async function main() {
 
   const totalSupply = promptMonsters.getMonstersTotalSupply();
   console.log(`totalSupply: ${totalSupply}`);
+
+  console.log("");
+  console.log(
+    "---------------------------------- owner address list for tokenID ----------------------------------",
+  );
+
+  const owners: any[] = [];
+
+  for (var i = 0; i < (await totalSupply).toNumber(); i++) {
+    owners[i] = promptMonsters.ownerOf(i);
+    console.log("No." + i + "  " + owners[i]);
+  }
+
+  console.log("");
+  console.log(
+    "---------------------------------- unique owner address list ----------------------------------",
+  );
+
+  const uniqueOwners: any[] = Array.from(new Set(owners));
+
+  for (var i = 0; i < uniqueOwners.length; i++) {
+    console.log("No." + i + "  " + uniqueOwners[i]);
+  }
+
+  console.log("");
+  console.log(
+    "---------------------------------- Initializing associative array ----------------------------------",
+  );
+
+  const object: { [key: string]: number } = {};
+
+  for (var i = 0; i < uniqueOwners.length; i++) {
+    object[uniqueOwners[i].toString()] = 0;
+  }
+
+  for (const key in object) {
+    const value = object[key];
+    console.log("key: [" + key + "] value:[" + value + "]");
+  }
+
+  console.log("");
+  console.log(
+    "---------------------------------- number of monsters for unique owner address ----------------------------------",
+  );
+
+  for (var key of Object.keys(object)) {
+    for (var j of owners) {
+      if (key == owners[j].toString()) {
+        object[key] = object[key] + 1;
+      }
+    }
+  }
+
+  for (const key in object) {
+    const value = object[key];
+    console.log("key: [" + key + "] value:[" + value + "]");
+  }
+
+  console.log("");
+  console.log(
+    "---------------------------------- send 10 OAS to unique owners ----------------------------------",
+  );
+
+  // 送金処理
 }
 
 main().catch((error) => {
