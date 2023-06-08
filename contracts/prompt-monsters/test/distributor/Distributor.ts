94dff77a32eede5cc432ab63122048b11fb42991
yosuke <yosuke2002jp@yahoo.co.jp>
2023-06-08 20:54:05 +0900

    謎のエラーと格闘中！

--- contracts/prompt-monsters/test/distributor/Distributor.ts
+++ contracts/prompt-monsters/test/distributor/Distributor.ts
@@ -1,4 +1,4 @@
-import { distibutor } from "../../typechain-types/contracts";
+import { distributor } from "../../typechain-types/contracts";
 import { deploy } from "./Deployment/Deployment";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
@@ -7,23 +7,19 @@ import { ethers } from "hardhat";
 describe("Distributor", function () {
   async function init() {
     const { distributor, promptMonsters, erc20 } = await loadFixture(deploy);
-
     const [deployer, user1, resurrectionPrompt1, resurrectionPrompt2] =
       await ethers.getSigners();
-
     expect(
       await distributor.grantRole(
         ethers.utils.id("DISTRIBUTOR_ROLE"),
         deployer.address,
       ),
     ).not.to.be.reverted;
-
     expect(
       await erc20
         .connect(deployer)
         .transfer(user1.address, ethers.utils.parseEther("10000")),
     ).not.to.be.reverted;
-
     return {
       distributor,
       promptMonsters,
@@ -45,15 +41,15 @@ describe("Distributor", function () {
     });
   });
 
-  describe("Set ERC20 token address", function () {
-    it("setERC20Address", async function () {
-      const { distributor, deployer, user1 } = await loadFixture(init);
-      expect(distributor.address).to.not.equal(
-        ethers.constants.AddressZero,
-        "zero address",
-      );
-    });
-  });
+  // describe("Set ERC20 token address", function () {
+  //   it("setERC20Address", async function () {
+  //     const { distributor, deployer, user1 } = await loadFixture(init);
+  //     expect(distributor.address).to.not.equal(
+  //       ethers.constants.AddressZero,
+  //       "zero address",
+  //     );
+  //   });
+  // });
 
   // describe("Check distributor", function () {
   //   it("calculateStamina", async function () {
