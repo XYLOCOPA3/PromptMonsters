992bb5f1ecef2bd0e06786b86c506c0c921602d6
yosuke <yosuke2002jp@yahoo.co.jp>
2023-06-09 07:38:27 +0900

    presentPerMint.tsを本番用に修正！

--- contracts/prompt-monsters/scripts/distributor/presentPerMint.ts
+++ contracts/prompt-monsters/scripts/distributor/presentPerMint.ts
@@ -7,34 +7,54 @@ export async function main() {
   console.log("account: ", deployer.address);
   console.log("from account: ", from.address);
 
-  // const PromptMonsters = await ethers.getContractFactory("PromptMonsters");
-  // const promptMonsters = PromptMonsters.attach(PROMPT_MONSTERS_PROXY_ADDRESS);
+  const PromptMonsters = await ethers.getContractFactory("PromptMonsters");
+  const promptMonsters = PromptMonsters.attach(PROMPT_MONSTERS_PROXY_ADDRESS);
+
   const Distributor = await ethers.getContractFactory("Distributor");
   const distributor = Distributor.attach(DISTRIBUTOR_PROXY_ADDRESS);
 
-  const totalSupply = 4; // 940;
-  console.log(`totalSupply: ${totalSupply}`);
+  const startBlockHeight = 21342235;
+  const endBlockHeight = 22542235;
 
-  const owners: (any | any)[][] = [
-    [0, "0x019281ce34F8b8739991713D5E09D0C290B53886"],
-    [1, "0x019281ce34F8b8739991713D5E09D0C290B53886"],
-    [2, "0x31F31693723c4397cb8A978A19A95B82c72f4212"],
-    [3, "0x4833C2fB6F00787c7F5f60a7F1a8aD9e191648C8"],
-  ];
+  const createLogsTmp = await promptMonsters.queryFilter(
+    promptMonsters.filters.MintedMonster(null, null, null),
+    startBlockHeight,
+    startBlockHeight + 1200000,
+  );
+
+  if (createLogsTmp.length === 0) return;
+
+  const owners: (any | any)[][] = [];
+
+  for (var i = 0; i < createLogsTmp.length; i++) {
+    owners.push([
+      createLogsTmp[i].args.newTokenId.toString(),
+      createLogsTmp[i].args.tokenOwner,
+    ]);
+    const t = await createLogsTmp[i].getBlock();
+  }
+
+  console.log("");
+  console.log(
+    "---------------------------------- display owners --------------------------------------------------------------",
+  );
+
+  for (var i = 0; i < owners.length; i++) {
+    console.log(owners[i][0] + ", " + owners[i][1]);
+  }
 
   console.log("");
   console.log(
     "---------------------------------- execute distribution for present per mint ----------------------------------",
   );
 
-  for (var i = 0; i < totalSupply; i++) {
+  for (var i = 0; i < owners.length; i++) {
     const distribution = await distributor //.connect(addr1).を使ってdistributor用のウォレットで関数を実行する
       .connect(from)
       .distributeNativeToken(owners[i][1], {
         value: ethers.utils.parseEther("0.01"),
       });
     await distribution.wait();
-    //console.log("[" + i + ',"' + (await promptMonsters.ownerOf(i)) + '"],');
   }
 
   console.log("");
