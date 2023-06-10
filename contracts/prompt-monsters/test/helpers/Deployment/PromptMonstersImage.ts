f107303b47c4115cb54628f0fbd6ea4e4855cccb
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-06-10 23:07:58 +0900

    feat: add BossBattle integration test

---
+++ contracts/prompt-monsters/test/helpers/Deployment/PromptMonstersImage.ts
@@ -0,0 +1,24 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers, upgrades } from "hardhat";
+
+export const deployPromptMonstersImage = async (
+  deployer: SignerWithAddress,
+  promptMonstersAddress: string,
+) => {
+  const PromptMonstersImage = await ethers.getContractFactory("PromptMonstersImage");
+  const promptMonstersImageProxy = await upgrades.deployProxy(
+    PromptMonstersImage,
+    [
+      promptMonstersAddress,
+    ],
+    {
+      kind: "uups",
+      initializer: "initialize",
+    },
+  );
+  await promptMonstersImageProxy.connect(deployer).deployed();
+
+  const promptMonstersImage = PromptMonstersImage.attach(promptMonstersImageProxy.address);
+
+  return { promptMonstersImage };
+};
